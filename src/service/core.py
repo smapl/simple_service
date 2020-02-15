bf9a34f2eb7f5b7bc98cd4fe3d2c0372df2d5b52
smapl <a.mustafin00@mail.ru>
2020-02-15 12:18:35 +0300

    patch

--- src/service/core.py
+++ src/service/core.py
@@ -1,3 +1,18 @@
 from flask import Flask
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+
+
+def find_user(user_id: str):
+    url = f"https://vk.com/id{user_id}"
+    resp = requests.get(url)
+
+    soup = BeautifulSoup(resp.text, "html.parser")
+    status = soup.find("div", {"class", "profile_online_lv"})
+
+    if status != "Online":
+        status = "Ofline"
+
+    return status
--- src/service/view.py
+++ src/service/view.py
@@ -1,5 +1,24 @@
 from .core import app
+from .core import find_user
+from flask import Flask, render_template, request, url_for, redirect
+
+vk_id = []
+
 
 @app.route("/")
-def main_page():
-    return "main page"
+def main():
+    try:
+        id_v = vk_id[-1]
+        status_vk = find_user(id_v)
+    except:
+        status_vk = ""
+    return render_template("index.html", status=status_vk)
+
+
+@app.route("/add_id")
+def add_id():
+    input_text = request.form["user_id"]
+    vk_id.append(input_text)
+
+    return redirect(url_for("main"))
+
